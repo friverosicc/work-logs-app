012d712595b5b9bc1752d031bd10de872f77b2f7
Francisco Riveros <friveros.icc@gmail.com>
2016-03-12 18:54:30 -0300

    refactorings

--- frontend/src/app/controller/login/login.js
+++ frontend/src/app/controller/login/login.js
@@ -14,15 +14,19 @@
         function($scope, $state, userResource, userSession) {
             $scope.login = function() {
                 userResource.login($scope.user.username, $scope.user.password)
-                .then(function() {
-                    userSession.save($scope.user);
-                    $state.go('app.work-logs', { username: $scope.user.username });
-                })
-                .catch(function(res) {
-                    $scope.error = res.data.msg;
-                    $scope.loginForm.password.$error = { invalidLogin: true };
-                });
+                .then(_validUserCredentials)
+                .catch(_invalidUserCredentials);
             };
+
+            function _validUserCredentials() {
+                userSession.save($scope.user);
+                $state.go('app.work-logs', { username: $scope.user.username });
+            }
+
+            function _invalidUserCredentials(response) {
+                $scope.error = response.data.msg;
+                $scope.loginForm.password.$error = { invalidLogin: true };
+            }
 
             $scope.isPossibleTryToLogin = function() {
                 return $scope.loginForm.$valid && $scope.loginForm.$dirty;
